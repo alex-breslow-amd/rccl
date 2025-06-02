90e4a4a655003ab502e29fbe61a90a01948385af
Alex Breslow <Alex.Breslow@amd.com>
2025-06-02 15:23:19 -0500

    Comment out waitcount

--- src/device/prims_simple.h
+++ src/device/prims_simple.h
@@ -189,9 +189,8 @@ private:
   inline __device__ void postPeer(bool dataStored) {
     if (Send && (flags & RolePostSend) && dataStored)
 #ifdef __GFX9__
-    //__threadfence();
-    atomic_thread_fence(memory_order_acquire, thread_scope_block);
-    __builtin_amdgcn_s_waitcnt(0);
+    __threadfence_block();
+    //__builtin_amdgcn_s_waitcnt(0);
 #else
     __threadfence_system();
 #endif
